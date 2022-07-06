d480fa864b6c0032e01308f046a81a4e4cb28efa
Rehtang <Warrior8584!>
2022-07-06 10:10:31 +0300

    git init

---
+++ src/main/java/ru/rehtang/second/ApsTestApplication.java
@@ -0,0 +1,11 @@
+package ru.rehtang.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ApsTestApplication.class, args);
+  }
+}
